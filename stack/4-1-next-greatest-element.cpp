619d998fa98055510a97b726de3c9f4f48652a51
surajSinsinwar <43957421+surajsinsinwar@users.noreply.github.com>
2020-08-27 23:02:32 +0530

    Add files via upload

---
+++ stack/4-1-next-greatest-element.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void nextGreatestElement(vector<int> v, int n)
+{
+	stack<int> st;
+
+    for(int i = 0; i <n ; i++)
+    {
+        while(!st.empty() && st.top() <= v[i])
+        {
+            st.pop();
+        }
+        st.empty() ? printf("-1 ") : printf("%d ", st.top());
+        st.push(v[i]);
+    }
+}
+
+int main()
+{
+	int n;
+    scanf("%d", &n);
+
+    vector<int> v(n);
+    for(int i = 0; i < n; i++)
+    {
+        scanf("%d", &v[i]);
+    }
+
+    nextGreatestElement(v, n);
+
+    return 0;
+}
---
+++ stack/4-2-next-greatest-element.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void nextGreatestElement(vector<int> v, int n)
+{
+    stack<int> st;
+    vector<int> ans(n);
+
+    for(int i = n - 1; i >= 0; i--)
+    {
+        while(!st.empty() && st.top() <= v[i])
+        {
+            st.pop();
+        }
+        ans[i] = st.empty() ? -1 : st.top();
+        st.push(v[i]);
+    }
+
+    for(int i = 0; i < n; i++)
+    {
+        printf("%d ", ans[i]);
+    }
+
+    return;
+}
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    vector<int> v(n);
+    for(int i = 0; i < n; i++)
+    {
+        scanf("%d", &v[i]);
+    }
+
+    nextGreatestElement(v, n);
+
+    return 0;
+}
